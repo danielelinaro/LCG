9d815bee10770aca27dc575dd9247b626393ea7d
danielelinaro <danielelinaro@aa9fed10-1944-4de4-aa28-cbea1cb38f0c>
2012-08-27 10:13:58 +0000

    Added minimal frontpage for the documentation.
    git-svn-id: svn+ssh://svn.code.sf.net/p/dynclamp/code/trunk@126 aa9fed10-1944-4de4-aa28-cbea1cb38f0c

--- src/common.h
+++ src/common.h
@@ -1,7 +1,39 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#define LOGFILE                 "/tmp/dynclamp.txt"
+/*!
+ * \file common.h
+ * \brief Header file with common definitions.
+ */
+
+/*!
+ * \mainpage A Dynamic Clamp library and toolbox (dynclamp)
+ *
+ * \author Daniele Linaro <daniele.linaro@ua.ac.be>
+ * \author Joao Couto <joao@tnb.ua.ac.be>
+ * \version 0.1
+ * \date 2012
+ * 
+ * \section con Contents:
+ *
+ * -# \ref intro "Introduction"
+ * -# \ref install "Installation"
+ * -# \ref examples "Examples of use"
+ * -# \ref links "Useful Links"
+ *
+ * \page intro Introduction
+ *
+ * \page install Installation
+ *
+ * \page examples Examples of use
+ *
+ * \page links Useful links
+ * This project uses the following external libraries:<br/>
+ * - <a href="http://www.comedi.org">Comedi</a> for data acquisition.
+ *
+ */
+
+ #define LOGFILE                 "/tmp/dynclamp.txt"
 //#define ASYNCHRONOUS_INPUT
 #define TRIM_ANALOGOUTPUT
 #endif
