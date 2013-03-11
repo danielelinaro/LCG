847dd6c4cc33f0dd508e9804f01acd631987c087
Daniele Linaro <danielelinaro@gmail.com>
2013-03-11 12:09:26 +0100

    Fixed the fclamp.py script and related configuration files.

--- bin/scripts/fclamp.py
+++ bin/scripts/fclamp.py
@@ -11,25 +11,27 @@ config_file = 'fclamp.xml'
 def usage():
     print('\nUsage: %s [option <value>]' % os.path.basename(sys.argv[0]))
     print('\nwhere options are:\n')
-    print('     -f    target frequency.')
-    print('     -d    stimulation duration (default 30 s).')
-    print('     -a    amplitude of the initially injected current (default 0 pA).')
-    print('     -p    proportional gain of the controller (default 0.01).')
-    print('     -i    integral gain of the controller (default 0.1).')
-    print('     -n    number of repetitions (default 1).')
-    print('     -w    interval between repetitions (default 30 s).')
-    print('     -I    input channel (default 0).')
-    print('     -O    output channel (default 0).')
-    print('\n --with-bg add background synaptic noise (default is no).')
+    print('     -f        Target frequency.')
+    print('     -T        Stimulation duration (default 30 s).')
+    print('     -t        Time constant of the frequency estimator (default 1 s).')
+    print('     -a        Amplitude of the initially injected current (default 0 pA).')
+    print('     -p        Proportional gain of the controller (default 0.01).')
+    print('     -i        Integral gain of the controller (default 0.1).')
+    print('     -d        Derivative gain of the controller (default 0).')
+    print('     -n        Number of repetitions (default 1).')
+    print('     -w        Interval between repetitions (default 30 s).')
+    print('     -I        Input channel (default 0).')
+    print('     -O        Output channel (default 0).')
+    print('  --with-bg    Add background synaptic noise (default is no).')
     print('\nThe following options are valid only if the --with-bg switch is used:\n')
-    print('     -R   input resistance of the cell (in MOhm).')
-    print('     -v   value of voltage at which the background activity should be balanced.')
-    print('     -F   firing frequency of the excitatory background population.')
+    print('     -R        Input resistance of the cell (in MOhm).')
+    print('     -v        Value of voltage at which the background activity should be balanced.')
+    print('     -F        Firing frequency of the excitatory background population.')
     print('')
-
+          
 def main():
     try:
-        opts,args = getopt.getopt(sys.argv[1:], 'hf:d:a:n:p:i:w:I:O:R:F:v:', ['help','with-bg'])
+        opts,args = getopt.getopt(sys.argv[1:], 'hf:T:t:a:n:p:i:d:t:w:I:O:R:F:v:', ['help','with-bg'])
     except getopt.GetoptError, err:
         print(str(err))
         usage()
@@ -41,7 +43,9 @@ def main():
     nreps = 1
     gp = 0.01
     gi = 0.1
+    gd = 0.
     duration = 30            # [s]
+    tau = 1                  # [s]
     interval = 30            # [s]
     I0 = 0                   # [pA]
     target_F = None          # [Hz]
@@ -55,8 +59,10 @@ def main():
             sys.exit(0)
         elif o == '-f':
             target_F = float(a)
-        elif o == '-d':
+        elif o == '-T':
             duration = float(a)
+        elif o == '-t':
+            tau = float(a)
         elif o == '-a':
             I0 = float(a)
         elif o == '-n':
@@ -79,6 +85,8 @@ def main():
             gp = float(a)
         elif o == '-i':
             gi = float(a)
+        elif o == '-d':
+            gd = float(a)
     
     if not target_F:
         print('You must specify specify the target frequency (-F switch).')
@@ -97,9 +105,9 @@ def main():
 
         ratio = dl.computeRatesRatio(balanced_voltage, input_resistance)
         Gm_exc,Gm_inh,Gs_exc,Gs_inh = dl.computeSynapticBackgroundCoefficients(ratio, input_resistance, R_exc)
-        dl.writeFClampConfig(target_F, I0, duration, gp, gi, config_file, ai, ao, with_bg, Gm_exc, Gs_exc, Gm_inh, Gs_inh)
+        dl.writeFClampConfig(target_F, I0, duration, gp, gi, gd, tau, config_file, ai, ao, with_bg, Gm_exc, Gs_exc, Gm_inh, Gs_inh)
     else:
-        dl.writeFClampConfig(target_F, I0, duration, gp, gi, config_file, ai, ao)
+        dl.writeFClampConfig(target_F, I0, duration, gp, gi, gd, tau, config_file, ai, ao)
 
     os.system('kernel_protocol -I ' + str(ai) + ' -O ' + str(ao))
     os.system('dclamp -c ' + config_file + ' -n ' + str(nreps) + ' -i ' + str(interval))
