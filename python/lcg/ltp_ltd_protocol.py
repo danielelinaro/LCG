780c48cc80ac4d050c440edca05c7543dd10e637
Daniele Linaro <danielelinaro@gmail.com>
2013-04-03 10:13:52 +0200

    Modified ltp_ltd_protocol.

--- python/lcg/ltp_ltd_protocol.py
+++ python/lcg/ltp_ltd_protocol.py
@@ -8,24 +8,64 @@ import lcg
 
 extracellular_stim_file = 'extra.stim'
 intracellular_stim_file = 'intra.stim'
-outfile = 'ltp_ltd.xml'
+config_file = 'ltp_ltd.xml'
 
 def usage():
     print('\nUsage: %s [option <value>]' % os.path.basename(sys.argv[0]))
     print('\nwhere options are:\n')
-    print('     -h     display this help message and exit.')
-    print('     -T     period of the stimulation.')
-    print('     -a     amplitude of intracellular pulse.')
-    print('     -n     number of repetitions.')
-    print('     -d     duration of each pulse (default 0.1 msec).')
+    print('     -h    display this help message and exit.')
+    print('     -T    period of the stimulation (in s).')
+    print('     -n    number of repetitions.')
+    print('     -s    duration of the extracellular stimulation (default 0.1 ms).')
+    print('     -I    input channel (default 0).')
+    print('     -O    output channels, in the form 0,1 where 0 and 1 are')
+    print('           the channels corresponding to the extracellular')
+    print('           and intracellular stimulation, respectively (default 0,1).')
+    print('\nIf the --pairing option is provided, a pairing protocol is performed and the')
+    print('following additional options are accepted:\n')
+    print('     -D    time difference between extracellular and intracellular pulse (in ms).')
+    print('     -a    amplitude of the intracellular pulse (in pA).')
+    print('     -d    duration of the intracellular pulse (defaul 1 ms).')
     print('')
-    print(' --pairing  pairing protocol.')
-    print('     -D     time difference between extracellular and intracellular pulse (in msec).')
-    print('')
+
+def run_batch(repetitions, interval, stim_dur, stim_amp, hyperpolarizing_pulse, pre, post,
+              ai, ao, pairing=False, offset=None):
+    try:
+        template_config_file = os.environ['CONFIGURATIONS_PATH'] + '/' + config_file
+    except:
+        template_config_file = os.environ['HOME'] + '/configurations/' + config_file
+    lcg.substituteStrings(template_config_file, config_file, {'TEND': pre+post,
+                                                              'AI': ai,
+                                                              'AO_INTRA': ao['intra'],
+                                                              'AO_EXTRA': ao['extra']})
+    stim_dur['extra'] = stim_dur['extra']*1e-3        # [s]
+        
+    extra_stim = [[pre,1,0,0,0,0,0,0,0,0,0,1],
+                  [stim_dur['extra'],1,stim_amp['extra'],0,0,0,0,0,0,0,0,1],
+                  [post-stim_dur['extra'],1,0,0,0,0,0,0,0,0,0,1]]
+    lcg.writeStimFile(extracellular_stim_file, extra_stim, False)
+
+    if pairing:
+        stim_dur['intra'] = stim_dur['intra']*1e-3    # [s]
+        offset = offset * 1e-3    # [s]
+        intra_stim = [[pre+offset,1,0,0,0,0,0,0,0,0,0,1],
+                      [stim_dur['intra'],1,stim_amp['intra'],0,0,0,0,0,0,0,0,1],
+                      [post-offset-stim_dur['intra']-hyperpolarizing_pulse['dur']-0.1,1,0,0,0,0,0,0,0,0,0,1],
+                      [hyperpolarizing_pulse['dur'],1,hyperpolarizing_pulse['amp'],0,0,0,0,0,0,0,0,1],
+                      [0.1,1,0,0,0,0,0,0,0,0,0,1]]
+    else:
+        intra_stim = [[pre+post-hyperpolarizing_pulse['dur']-0.1,1,0,0,0,0,0,0,0,0,0,1],
+                      [hyperpolarizing_pulse['dur'],1,hyperpolarizing_pulse['amp'],0,0,0,0,0,0,0,0,1],
+                      [0.1,1,0,0,0,0,0,0,0,0,0,1]]
+
+    lcg.writeStimFile(intracellular_stim_file, intra_stim, False)
+
+    os.system('kernel_protocol -I ' + str(ai) + ' -O ' + str(ao['intra']))
+    os.system('dclamp -c '+ config_file + ' -n ' + str(repetitions) + ' -i ' + str(interval-(pre+post)))
 
 def main():
     try:
-        opts,args = getopt.getopt(sys.argv[1:], 'hT:n:d:a:D:',
+        opts,args = getopt.getopt(sys.argv[1:], 'hT:n:s:D:a:d:I:O:',
                                   ['help','pairing'])
     except getopt.GetoptError, err:
         print str(err)
@@ -37,15 +77,16 @@ def main():
     post = 1              # [s]
     interval = None       # [s]
     ai = 0
-    intra_ao = 0
-    extra_ao = 1
+    ao = {'intra': 0, 'extra': 1}
     stim_freq = None      # [Hz]
-    extra_stim_dur = 0.1  # [ms]
-    intra_stim_dur = 1    # [ms]
-    extra_stim_amp = 5    # [V]
-    intra_stim_amp = None # [pA]
+    stim_dur = {'extra': 0.1, 'intra': 1}  # [ms]
+    stim_amp = {'extra': 5,    # [V]
+                'intra': None} # [pA]
     pairing = False
     offset = None         # [pA]
+
+    hyperpolarizing_pulse = {'dur': 0.1,  # [s]
+                             'amp': -100} # [pA]
 
     for o,a in opts:
         if o in ('-h','--help'):
@@ -54,57 +95,65 @@ def main():
         elif o == '-T':
             interval = float(a)
         elif o == '-n':
-            repetitions = int(a)
-        elif o == '-d':
-            extra_stim_dur = float(a)
+            repetitions = []
+            for rep in a.split(','):
+                repetitions.append(int(rep))
+        elif o == '-s':
+            stim_dur['extra'] = float(a)
         elif o == '-D':
             offset = float(a)
         elif o == '-a':
-            intra_stim_amp = float(a)
+            stim_amp['intra'] = float(a)
+        elif o == '-d':
+            stim_dur['intra'] = float(a)
         elif o == '--pairing':
             pairing = True
-
-    if pairing and not offset:
-        print('You must specify the time difference between extracellular and intracellular pulses (-D switch).')
-        sys.exit(1)
-
-    if pairing and not intra_stim_amp:
-        print('You must specify the amplitude of the intracellular pulse (-a switch).')
-        sys.exit(1)
-
-    if not interval:
-        print('You must specify the period of the stimulation (-T switch).')
-        sys.exit(1)
+        elif o == '-I':
+            ai = int(a)
+        elif o == '-O':
+            channels = a.split(',')
+            ao['intra'] = channels[0]
+            ao['extra'] = channels[1]
 
     if not repetitions:
         print('You must specify the number of repetitions (-n switch).')
         sys.exit(1)
 
-    extra_stim_dur = extra_stim_dur * 1e-3    # [s]
+    if len(repetitions) > 3:
+        print('Don''t know what to do with %d batches.' % len(repetitions))
+        sys.exit(1)
 
-    extra_stim = [[pre,1,0,0,0,0,0,0,5061983,0,0,1],
-                  [extra_stim_dur,1,extra_stim_amp,0,0,0,0,0,5061983,0,0,1],
-                  [post-extra_stim_dur,1,0,0,0,0,0,0,5061983,0,0,1]]
-    lcg.writeStimFile(extracellular_stim_file, extra_stim, False)
+    if len(repetitions) > 1:
+        pairing = True
 
     if pairing:
-        intra_stim_dur = intra_stim_dur * 1e-3    # [s]
-        offset = offset * 1e-3    # [s]
-        intra_stim = [[pre+offset,1,0,0,0,0,0,0,5061983,0,0,1],
-                      [intra_stim_dur,1,intra_stim_amp,0,0,0,0,0,5061983,0,0,1],
-                      [post-offset-intra_stim_dur,1,0,0,0,0,0,0,5061983,0,0,1]]
-        lcg.writeStimFile(intracellular_stim_file, intra_stim, False)
-        os.system('kernel_protocol -I ' + str(ai) + ' -O ' + str(intra_ao))
-        try:
-            infile = os.environ['CONFIGURATIONS_PATH'] + '/' + outfile
-        except:
-            infile = os.environ['HOME'] + '/configurations/' + outfile
-        lcg.substituteStrings(infile, outfile, {'TEND': pre+post})
-        os.system('dclamp -c '+ outfile + ' -n ' + str(repetitions) + ' -i ' + str(interval-(pre+post)))
-    else:
-        os.system('cclamprc_write -e -i -c ' + str(ai))
-        os.system('cclamprc_write -o -f 1 -u V -c ' + str(extra_ao))
-        os.system('cclamp -f ' + extracellular_stim_file + ' -n ' + str(repetitions) + ' -i ' + str(interval-(pre+post)))
+        if not offset:
+            print('You must specify the time difference between extracellular and intracellular pulses (-D switch).')
+            sys.exit(1)
+        if not stim_amp['intra']:
+            print('You must specify the amplitude of the intracellular pulse (-a switch).')
+            sys.exit(1)
 
+    if not interval:
+        print('You must specify the period of the stimulation (-T switch).')
+        sys.exit(1)
+
+    if len(repetitions) == 1:
+        run_batch(repetitions[0], interval, stim_dur, stim_amp, hyperpolarizing_pulse, pre, post, ai, ao, pairing, offset)
+    else:
+        os.mkdir('01',0755)
+        os.chdir('01')
+        run_batch(repetitions[0], interval, stim_dur, stim_amp, hyperpolarizing_pulse, pre, post, ai, ao, False)
+        os.chdir('..')
+        os.mkdir('02',0755)
+        os.chdir('02')
+        run_batch(repetitions[1], interval, stim_dur, stim_amp, hyperpolarizing_pulse, pre, post, ai, ao, True, offset)
+        os.chdir('..')
+        if len(repetitions) == 3:
+            os.mkdir('03',0755)
+            os.chdir('03')
+            run_batch(repetitions[2], interval, stim_dur, stim_amp, hyperpolarizing_pulse, pre, post, ai, ao, False)
+            os.chdir('..')
+        
 if __name__ == '__main__':
     main()
