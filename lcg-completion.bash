8826ed79faaad3fd4ca57878f9fb7876193baad1
Daniele Linaro <danielelinaro@gmail.com>
2013-06-11 15:10:14 +0200

    Added bash script for subcommand auto completion.

---
+++ lcg-completion.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+_lcg()
+{
+    local cur prev subcommands
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    subcommands="annotate ap correlations cv experiment extracellular fclamp fi fopt help kernel ltpltd ou output pspopt pulse pulses ramp rate-steps sinusoids steps tau test_daq vcclamp vi zero"
+    if [[ ${prev} == "lcg" ]] ; then
+        COMPREPLY=( $(compgen -W "${subcommands}" -- ${cur}) )
+        return 0
+    fi
+}
+
+complete -F _lcg lcg
+
